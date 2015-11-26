b3c041235d76a271eb51d971c489d84f0a541886
Colin Gillespie <csgillespie@gmail.com>
2015-11-26 17:31:59 +0000

    Brief docs for upload_results

--- R/upload_results.R
+++ R/upload_results.R
@@ -1,10 +1,11 @@
-#' @title Upload results
+#' @title Upload benchmark results
+#' @param results Benchmark results. Probably obtained from \code{benchmark_all()}.
+#' @param url The location of where to upload the results.
 #' @export
 #' @importFrom httr POST upload_file
-upload_results = function(results) {
+upload_results = function(results, url="http://www.mas.ncl.ac.uk/~ncsg3/form.php") {
   type = get_sys_details()
   type$results = results
-  url = "http://www.mas.ncl.ac.uk/~ncsg3/form.php"
   fname = tempfile(fileext=".RData")
   saveRDS(results, file=fname)
   r = POST(url, 
