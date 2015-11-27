f4c16b3cd1f575f94267f054b2395e595af66673
Colin Gillespie <csgillespie@gmail.com>
2015-11-27 16:51:56 +0000

    Update benchmark test

--- R/sys_info.R
+++ R/sys_info.R
@@ -1,12 +1,26 @@
 #' Byte compiler status
 #' 
-#' Attempts to detect if byte compiling has been used on the package.
+#' Attempts to detect if byte compiling has been used on the package. There isn't a fool proof
+#' way of detecting if the the package has been benchmarked. 
 #' @return An integer indicating if byte compiling has been turn on. See \code{?compiler} for
 #' details.
 #' @importFrom compiler getCompilerOption
 #' @export
 get_byte_compiler = function() {
-  structure(Sys.getenv("R_COMPILE_PKGS"), names = "byte_optimize")
+  comp = Sys.getenv("R_COMPILE_PKGS")
+  
+  ## Try to detect compilePKGS - long shot
+  ## Return to same state as we found it
+  if(length(nchar(comp)) == 0L) {
+    comp = compiler::compilePKGS(1)
+    compiler::compilePKGS(comp)
+    if(comp) {
+      comp = compiler::getCompilerOption("optimize")
+    } else {
+      comp = 0
+    }
+  }
+  structure(comp, names = "byte_optimize")
 }
 
 
