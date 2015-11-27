dc6ba9c1379039cffe37bb250b2c2c1e83776446
Colin Gillespie <csgillespie@gmail.com>
2015-11-27 11:56:11 +0000

    Adding initial benchmark data

---
+++ R/data_help_files.R
@@ -0,0 +1,8 @@
+#' @rdname data_results
+#' @name results
+#' @title Benchmarking results
+#' @description A summary of past benchmarks. The full data set will be 
+#' made available in a companion package
+#' @docType data
+#' @format A data frame
+NULL
