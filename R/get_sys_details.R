b09b4a20d263d9f3f01ee6459695a9da18004e90
Colin Gillespie <csgillespie@gmail.com>
2015-11-30 16:52:15 +0000

    Silly ifelse bug

--- R/get_sys_details.R
+++ R/get_sys_details.R
@@ -32,14 +32,30 @@ get_sys_details = function(sys_info = TRUE, platform_info = TRUE,
                            cpu=TRUE, byte_compiler=TRUE, linear_algebra=TRUE,
                            installed_packages=TRUE) {
   l = list()
-  l$sys_info = ifelse(sys_info, as.list(Sys.info()), NA)
-  l$platform_info = ifelse(platform_info, get_platform_info(), NA)
-  l$r_version = ifelse(r_version, get_r_version(), NA)
-  l$ram = ifelse(ram, as.list(get_ram()), NA)
-  l$cpu = ifelse(cpu, get_cpu(), NA)
-  l$byte_compiler = ifelse(byte_compiler, as.list(get_byte_compiler()), NA)
-  l$linear_algebra = ifelse(linear_algebra, get_linear_algebra(), NA)
-  l$installed_packages = ifelse(installed_packages, installed.packages(), NA)
+  if(sys_info) l$sys_info = as.list(Sys.info())
+  else l$sys_info = NA
+  
+  if(platform_info) l$platform_info = get_platform_info()
+  else  l$platform_info = NA
+  
+  if(r_version) l$r_version = get_r_version()
+  else l$r_version = NA
+  
+  if(ram) l$ram = get_ram()
+  else l$ram  = NA
+  
+  if(cpu) l$cpu = get_cpu()
+  else l$cpu = NA
+  
+  if(byte_compiler) l$byte_compiler = get_byte_compiler()
+  else l$byte_compiler = NA
+  
+  if(linear_algebra) l$linear_algebra = get_linear_algebra()
+  else l$linear_algebra = NA
+  
+  if(installed_packages)   l$installed_packages  = installed.packages()
+  else   l$installed_packages = NA
+  
   l$package_version = packageDescription("benchmarkme")$Version
   l$id = paste0(Sys.Date(), "-", sample(1e8, 1))
   l$date = structure(Sys.Date(), class="Date")
