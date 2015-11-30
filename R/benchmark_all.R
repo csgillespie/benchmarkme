d940fafb82669f3f507b4a07450f328dc85f0be0
Colin Gillespie <csgillespie@gmail.com>
2015-11-30 13:32:45 +0000

    Minor: space

--- R/benchmark_all.R
+++ R/benchmark_all.R
@@ -14,7 +14,7 @@ benchmark_all = function(runs=3, verbose=TRUE) {
 
 #' @rdname benchmark_all
 #' @export
-get_benchmarks = function(){
+get_benchmarks = function() {
   prog_funs = c("bm_prog_fib", "bm_prog_gcd", "bm_prog_hilbert", "bm_prog_toeplitz", "bm_prog_escoufier")
   matrix_funs = c("bm_matrix_cholesky", "bm_matrix_cross_product", "bm_matrix_lm", 
                   "bm_matrix_determinant", "bm_matrix_eigen", "bm_matrix_fft", 
