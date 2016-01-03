aa0a1d892b97d4fe79da350d9ec121ce51e77a41
Colin Gillespie <csgillespie@gmail.com>
2016-01-03 22:24:43 +0000

    Moved plot_past and get_datatable_past to benchmarkmeData

--- R/datatable.R
+++ R/datatable.R
@@ -3,7 +3,6 @@
 #' Compare your results against past results. Your results is 
 #' shown in Orange.
 #' @inheritParams upload_results
-#' @import benchmarkmeData
 #' @export
 get_datatable = function(results) {
   if(!requireNamespace("DT", quietly = TRUE))
@@ -42,38 +41,6 @@ get_datatable = function(results) {
                   backgroundColor = DT::styleEqual(current_rank, "orange"))
 }
 
-#' @rdname get_datatable
-#' @inheritParams plot_past
+#' @importFrom benchmarkmeData get_datatable_past
 #' @export
-get_datatable_past = function(byte_optimize=NULL) {
-  if(!requireNamespace("DT", quietly = TRUE))
-    stop("Install DT package to use datatable")
-  
-  
-  ## Load past data
-  tmp_env = environment()
-  data(past_results, package="benchmarkmeData", envir = tmp_env)
-  results = tmp_env$past_results
-  results$cpus = as.character(results$cpus)
-  
-  if(!is.null(byte_optimize)){
-    if(byte_optimize) {
-      results = results[results$byte_optimize > 0.5,]
-    } else {
-      results = results[results$byte_optimize < 0.5,]
-    }
-  }
-  
-  results$timings = signif(results$timings, 5)
-  results = results[order(results$timings), ]
-  results$rank = 1:nrow(results)
-  
-  if(is.null(byte_optimize)){
-    results = results[,c("rank", "cpus", "timings", "byte_optimize")]
-    colnames(results) = c("Rank", "CPU", "Time (sec)", "Byte Compile")
-  } else {
-    results = results[,c("rank", "cpus", "timings")]
-    colnames(results) = c("Rank", "CPU", "Time (sec)")
-  }
-  DT::datatable(results, rownames=FALSE) 
-}
+benchmarkmeData::get_datatable_past
--- R/plot_results.R
+++ R/plot_results.R
@@ -52,48 +52,6 @@ plot.ben_results = function(x, log="y", ...) {
 }
 
 
-#' Plot past results
-#' 
-#' Plot the previous benchmarks. This function creates two figures.
-#' \itemize{
-#' \item Figure 1: Total benchmark time over all benchmarks (in seconds) on the y-axis..
-#' \item Figure 2: Relative time (compared to the smallest benchmark).
-#' }
-#' The data set used is \code{data(past_results)}.
-#' @param byte_optimize Default \code{NULL}. The default behaviour is to plot all results.
-#' To plot only the byte optimized results, set to \code{TRUE}, otherwise \code{FALSE}.
-#' @param log By default the y axis is plotted on the log scale. To change, set the 
-#' the argument equal to the empty parameter string, \code{""}.
-#' @examples 
-#' ## Plot non byte optimize code
-#' plot_past(byte_optimize=FALSE)
+#' @importFrom benchmarkmeData plot_past
 #' @export
-plot_past = function(byte_optimize = NULL, log="y") {
-  ## Load past data
-  tmp_env = new.env()
-  data(past_results, package="benchmarkmeData", envir = tmp_env)
-  pas_res = tmp_env$past_results
-  pas_res = pas_res[order(pas_res$timings), ]
-  if(!is.null(byte_optimize) && byte_optimize)
-    pas_res = pas_res[pas_res$byte_optimize > 0.5,]
-  else if(!is.null(byte_optimize) && !byte_optimize)
-    pas_res = pas_res[pas_res$byte_optimize < 0.5,]
-  
-  ## Sort plot
-  op = par(mar=c(3,3,2,1), 
-           mgp=c(2,0.4,0), tck=-.01,
-           cex.axis=0.8, las=1, mfrow=c(1,2)) 
-  on.exit(op)
-  ymax = max(pas_res$timings)
-  plot(pas_res$timings, xlab="Rank", ylab="Total timing (secs)", 
-       ylim=c(1, ymax), xlim=c(1, nrow(pas_res)+1), 
-       panel.first=grid(), log=log)
-  
-  ## Relative timings  
-  fastest = min(pas_res$timings)
-  ymax= ymax/fastest
-  plot(pas_res$timings/fastest, xlab="Rank", ylab="Relative timing", 
-       ylim=c(1, ymax), xlim=c(1, nrow(pas_res)+1), 
-       panel.first=grid(), log=log)
-  abline(h=1, lty=3)
-}
+benchmarkmeData::plot_past
