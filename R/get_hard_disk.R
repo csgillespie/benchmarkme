52a05b5608e84c1fef23748612b2748499066a45
Colin Gillespie <csgillespie@gmail.com>
2016-01-06 13:53:18 +0000

    Place holder.

---
+++ R/get_hard_disk.R
@@ -0,0 +1,18 @@
+## A work in progress
+# get_hard_drive = function() {
+#   if(Sys.info()["sysname"]=="Windows") {
+#     cmd = "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe get-wmiobject win32_diskdrive"
+#     hard_disk = system(cmd, intern=TRUE)
+#   }
+# }
+
+
+# cmd = "C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe get-wmiobject win32_diskdrive"
+# system(cmd)
+# 
+# 
+# Partitions : 2
+# DeviceID   : \\.\PHYSICALDRIVE0
+# Model      : SAMSUNG MZ7PD128HCFV-000 SCSI Disk Device
+# Size       : 128034708480
+# Caption    : SAMSUNG MZ7PD128HCFV-000 SCSI Disk Device
