d8013be172a24c9d1c4c4bfccd8f36a02f70aef0
Colin Gillespie <csgillespie@gmail.com>
2016-06-20 14:29:20 +0100

    Refactoring

--- R/get_blas_library.R
+++ R/get_blas_library.R
@@ -1,6 +1,6 @@
 #' Get BLAS and LAPACK libraries
 #' 
-#' This currently only works under Linux. For other OS, \code{NA}'s 
+#' This currently only works under Linux and Apple OS. For other OS, e.g. Windows \code{NA}'s 
 #' are returned.
 #' 
 #' @details Calls `Sys.getpid()` and greps blas/lapack
@@ -8,15 +8,18 @@
 get_linear_algebra = function() {
   ##XXX: ?R CMD config BLAS_LIBS
   os = R.version$os
-  if(length(grep("^linux", os))) {
+  if(length(grep("^linux", os)) || length(grep("^darwin", os))) {
     p_id = Sys.getpid()
     cmd_blas = paste("lsof -p", p_id, "| grep 'blas' | awk '{print $9}'")
     cmd_lapack = paste("lsof -p", p_id, "| grep 'lapack' | awk '{print $9}'")
-    blas = system(cmd_blas, intern=TRUE)
-    lapack = system(cmd_lapack, intern=TRUE)
+    blas = try(system(cmd_blas, intern=TRUE), silent=TRUE)
+    lapack = try(system(cmd_lapack, intern=TRUE), silent=TRUE)
+    if(class(blas) == "try-error") {
+      blas = lapack = NA
+    }
   } else {
-    blas = NA
-    lapack=NA
+    blas = lapack = NA
   }
   list(blas = blas, lapack=lapack)
 }
+
