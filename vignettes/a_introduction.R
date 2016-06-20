14df55f82d8a2eb59355195e524061adc6ae47fb
Colin Gillespie <csgillespie@gmail.com>
2016-06-20 16:39:09 +0100

    Update vignette

--- vignettes/a_introduction.R
+++ vignettes/a_introduction.R
@@ -11,13 +11,12 @@ data(past_results, package="benchmarkmeData")
 ## ------------------------------------------------------------------------
 head(past_results, 3)
 
-## ---- fig.width=7, fig.height=3.5----------------------------------------
+## ---- fig.width=7, fig.height=3.5, message=FALSE-------------------------
 plot_past()
 ## shine() # if you have shiny
 
-## ---- fig.width=7, fig.height=3.5----------------------------------------
+## ---- fig.width=7, fig.height=3.5, message=FALSE, results="hide"---------
 plot(res)
-## shine(res) # if you have shiny
 
 ## ----eval=FALSE----------------------------------------------------------
 #  install.packages("DT")
@@ -26,4 +25,16 @@ plot(res)
 #  ## Your result is highlighted in orange.
 #  get_datatable(res)
 #  get_datatable_past()
+
+## ----eval=FALSE----------------------------------------------------------
+#  res_io = benchmark_io()
+#  upload_results(res_io)
+#  ## Sample size is small, so don't split by byte/blas
+#  plot(res_io, byte_optimize=NULL, blas_optimize=NULL)
+
+## ----eval=FALSE----------------------------------------------------------
+#  Sys.getenv("TMPDIR")
+
+## ----eval=FALSE----------------------------------------------------------
+#  res_io = benchmark_io(tmpdir="some_other_directory")
 
