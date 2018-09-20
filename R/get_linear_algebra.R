e0e1d19e01127c3d7fb6792065a2a3bbd80fc8f8
Colin Gillespie <csgillespie@gmail.com>
2018-09-20 17:11:37 +0100

    Minimal R version now 3.5. Avoids btye compiler issues

--- R/get_linear_algebra.R
+++ R/get_linear_algebra.R
@@ -1,54 +1,11 @@
-la_helper = function() {
-  p_id = Sys.getpid()
-  # Get all R processes
-  lsof_cmd = paste("lsof -p", p_id, "| awk '{print $9}'")
-  lsof = try(system(lsof_cmd, intern = TRUE), silent = TRUE)
-  
-  # Grep for BLAS; should pick up the mkl BLAS as well
-  blas = lsof[grep("blas", x = lsof)]
-  if(class(blas) == "try-error" || length(blas) == 0) 
-    blas = NA
-  
-  # Now lapack; try standard, then the libmkl
-  lapack = lsof[grep("lapack", x = lsof)]
-  
-  if(length(lapack) == 0)
-    lapack = lsof[grep("libmkl_.*lp.*\\.so$", lsof)]
-  
-  if(class(lapack) == "try-error" || length(lapack) == 0)
-    lapack = NA
-  
-  ## NA returned if BLAS is hiding
-  return(list(blas = blas, lapack = lapack))
-}
-
-
-
 #' Get BLAS and LAPACK libraries
+#' Extract the the blas/lapack from sessionIfno 
 #' 
-#' This currently only works under Linux and Apple OS. For other OS, e.g. Windows \code{NA}'s 
-#' are returned.
-#' 
-#' @details Calls `Sys.getpid()` and greps for blas,lapack and libmkl. 
-#' Under Linux a warning is raised 
-#' "lsof: WARNING: can't stat() tracefs file system /sys/kernel/debug/tracing Output information may be incomplete."
-#' There seems to be no way to avoid this http://askubuntu.com/q/748498/1986
 #' @export
 get_linear_algebra = function() {
-  r_version = paste(R.version$major, R.version$minor, sep=".")
-  if(compareVersion(r_version, "3.4.0") >=0) {
     s = sessionInfo()
     blas = s$BLAS
     lapack = s$LAPACK
     return(list(blas = blas, lapack = lapack))
-  }
-  
-  os = R.version$os
-  if(length(grep("^linux", os)) || length(grep("^darwin", os))) {
-    linear_algebra = la_helper()
-  } else {
-    linear_algebra = list(blas = NA, lapack = NA) # nocov
-  }
-  return(linear_algebra)
 }
 
