38a477aaafe87068cf7b77b0ce0f0fd3282200d1
Colin Gillespie <csgillespie@gmail.com>
2019-01-18 16:17:37 +0000

    Remove shiny link until we've updated it

--- R/zzz.r
+++ R/zzz.r
@@ -1,7 +1,7 @@
 #' @importFrom stats na.omit
 .onAttach = function(...) {
-  if (!interactive()) return()
-
-  msg = "See https://jumpingrivers.shinyapps.io/benchmarkme/ for a Shiny interface to the benchmark data." # nocov
-  packageStartupMessage(paste(strwrap(msg), collapse = "\n")) # nocov
+  # if (!interactive()) return()
+  # 
+  # msg = "See https://jumpingrivers.shinyapps.io/benchmarkme/ for a Shiny interface to the benchmark data." # nocov
+  # packageStartupMessage(paste(strwrap(msg), collapse = "\n")) # nocov
 }
